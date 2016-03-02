c8021c912bc2e921e0284ba598c36d75c75edf79
YipManDan <Yeh.DanielP@gmail.com>
2016-03-01 17:05:06 -0800

    FileTransferHandler: Include accept file and write to temporary file

--- src/FileTransferHandler.java
+++ src/FileTransferHandler.java
@@ -1,16 +1,67 @@
+import java.io.*;
+import java.net.Socket;
 import java.util.ArrayList;
 
 /**
  * Created by Daniel on 2/29/2016.
+ * Reference: http://www.rgagnon.com/javadetails/java-0542.html
  */
 public class FileTransferHandler {
 
     private ArrayList<UserId> recipients;
+    private ChatMessage cMsg;
     private int transferId;
+    ChatServer.ClientThread ct;
 
-    FileTransferHandler(ArrayList<UserId> recipients, int transferId) {
-        this.recipients = recipients;
+
+    FileTransferHandler(ChatMessage cMsg, int transferId, ChatServer.ClientThread ct) {
+        this.cMsg = cMsg;
         this.transferId = transferId;
+        this.recipients = cMsg.getRecipients();
+        this.ct = ct;
+        long length = cMsg.getFileSize();
+        UserId sender = cMsg.getSender();
+
+        int bytesRead;
+        int current = 0;
+        FileOutputStream fos = null;
+        BufferedOutputStream bos = null;
+        Socket socket = ct.socket;
+
+        //This should accept the file into a temporary file
+        try {
+            byte [] mybytearray = new byte [((int) length)];
+            InputStream is = socket.getInputStream();
+            File tempFile = File.createTempFile("tmp", null, null);
+            fos = new FileOutputStream(tempFile);
+            bos = new BufferedOutputStream(fos);
+            bytesRead = is.read(mybytearray, 0, mybytearray.length);
+            current = bytesRead;
+            do {
+                bytesRead = is.read(mybytearray, current, (mybytearray.length-current));
+                if(bytesRead >= 0)
+                    current += bytesRead;
+            } while (bytesRead > -1);
+
+            bos.write(mybytearray, 0, current);
+            bos.flush();
+            //Print out success alert
+
+        }
+        catch (IOException e){
+            //QQ
+        }
+        finally {
+            try {
+                if (fos != null)
+                    fos.close();
+                if(bos != null)
+                    bos.close();
+            }
+            catch (IOException e){
+                //QQ
+            }
+        }
     }
 
     int getTransferId(){
